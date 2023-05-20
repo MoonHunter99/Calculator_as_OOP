f7d0472ed9825d030a23461abc1752b44da8b2f5
Neil Daniel Vergara <vergaraneildaniel@gmail.com>
2023-05-20 20:56:58 +0800

    Addition method is now created in class_calculator

--- class_calculator.py
+++ class_calculator.py
@@ -1,5 +1,9 @@
 #create a class for calculator uses
+class Calculator:
     #method Add
+    def Add(self, number1 , number2):
+        sum = number1 + number2
+        return sum
     #method Subtract
     #method Multiply
     #method Divide
