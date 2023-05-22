1dcaa17d9885f0025e9a679e0f0bafe09474d946
Neil Daniel Vergara <vergaraneildaniel@gmail.com>
2023-05-22 20:08:35 +0800

    Subtraction method complete

--- class_calculator.py
+++ class_calculator.py
@@ -1,10 +1,13 @@
 #create a class for calculator uses
 class Calculator:
     #method Add
-    def Add(self, number1 , number2):
+    def add(self, number1 , number2):
         sum = number1 + number2
         return sum
     #method Subtract
+    def minus(self, number1 , number2):
+        difference = number1 - number2
+        return difference
     #method Multiply
     #method Divide
     #print Sum
