194ad25f8a79399f5c474c9f62701f04d1a4f8be
Neil Daniel Vergara <vergaraneildaniel@gmail.com>
2023-05-22 20:34:55 +0800

    printing answers complete

--- class_calculator.py
+++ class_calculator.py
@@ -17,6 +17,14 @@ class Calculator:
         quotient = number1 / number2
         return quotient
     #print Sum
+    def print_sum(self, sum):
+        print("The sum is: " , sum)
     #print Difference
+    def print_difference(self, difference):
+        print("The difference is: " , difference)
     #print Product
+    def print_product(self, product):
+        print("The product is: " , product)
     #print Quotient
+    def print_quotient(self, quotient):
+        print("The quotient is: " , quotient)
