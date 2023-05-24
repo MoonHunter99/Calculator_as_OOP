5ff19d534f5cf1ceb0616f6ad5be5518678a0f37
Neil Daniel Vergara <vergaraneildaniel@gmail.com>
2023-05-24 15:59:22 +0800

    asking operation and checking and printing done and working

--- main_calculator.py
+++ main_calculator.py
@@ -9,5 +9,18 @@ first_number = ui.ask_number()
 #get the second number
 second_number = ui.ask_number()
 #ask the user what method to use
+operation = ui.ask_operation()
 #check using if else on what method in calculator to use
 #print the result
+if operation == "+" or "add":
+    sum = calc.add(first_number , second_number)
+    calc.print_sum(sum)
+elif operation == "-" or "minus":
+    difference = calc.minus(first_number , second_number)
+    calc.print_difference(difference)
+elif operation == "*" or "multiply":
+    product = calc.multiply(first_number, second_number)
+    calc.print_product(product)
+elif operation == "/" or "divide":
+    quotient = calc.divide(first_number , second_number)
+    calc.print_quotient(quotient)
