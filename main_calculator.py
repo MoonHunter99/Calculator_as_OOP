74360e3289bae9571816362de3d597da5377e523
Neil Daniel Vergara <vergaraneildaniel@gmail.com>
2023-05-24 15:57:59 +0800

    asking 2 numberss complete and working

--- main_calculator.py
+++ main_calculator.py
@@ -5,7 +5,9 @@ from user_interface import UserInterface
 ui = UserInterface("Calculator")
 calc = Calculator()
 #get the first numbers
+first_number = ui.ask_number()
 #get the second number
+second_number = ui.ask_number()
 #ask the user what method to use
 #check using if else on what method in calculator to use
 #print the result
