fa37f955cb871dc90c26ec11e0e292b71fdf2481
Neil Daniel Vergara <vergaraneildaniel@gmail.com>
2023-05-24 11:39:07 +0800

    Created a constructor in user interface

--- user_interface.py
+++ user_interface.py
@@ -1,4 +1,8 @@
 #create a class UserInterface
+class UserInterface:
     #constructor of greetings and program description
+    def __init__(self , item):
+        print("Hello welcome to my programmed " , item)
+        print("\033[1;39m `"* 112)
     #method getting the number
     #method for asking the what to do in calculator
