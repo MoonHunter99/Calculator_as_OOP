cd49c317d9dbe022d606d533aac8093d6bda9152
Neil Daniel Vergara <vergaraneildaniel@gmail.com>
2023-06-03 20:52:07 +0800

    Main program is now edited and is using the Child class which inherited the UserInterface and Calculator classes

--- main_calculator.py
+++ main_calculator.py
@@ -1,26 +1,24 @@
 #call all the class that is needed
-from class_calculator import Calculator
-from user_interface import UserInterface
+from daniel_calculator import DanielCalculator
 #create a instance object for the classes
-ui = UserInterface("Calculator")
-calc = Calculator()
+daniel = DanielCalculator("Cassio")
 #get the first numbers
-first_number = ui.ask_number()
+first_number = daniel.ask_number()
 #get the second number
-second_number = ui.ask_number()
+second_number = daniel.ask_number()
 #ask the user what method to use
-operation = ui.ask_operation()
+operation = daniel.ask_operation()
 #check using if else on what method in calculator to use
 #print the result
 if operation == "+" or "add":
-    sum = calc.add(first_number , second_number)
-    calc.print_sum(sum)
+    sum = daniel.add(first_number , second_number)
+    daniel.print_sum(sum)
 elif operation == "-" or "minus":
-    difference = calc.minus(first_number , second_number)
-    calc.print_difference(difference)
+    difference = daniel.minus(first_number , second_number)
+    daniel.print_difference(difference)
 elif operation == "*" or "multiply":
-    product = calc.multiply(first_number, second_number)
-    calc.print_product(product)
+    product = daniel.multiply(first_number, second_number)
+    daniel.print_product(product)
 elif operation == "/" or "divide":
-    quotient = calc.divide(first_number , second_number)
-    calc.print_quotient(quotient)
+    quotient = daniel.divide(first_number , second_number)
+    daniel.print_quotient(quotient)
