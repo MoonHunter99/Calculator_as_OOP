bda216c9d5fe48783c05379d1c16b27b83884693
Neil Daniel Vergara <vergaraneildaniel@gmail.com>
2023-06-03 21:09:23 +0800

    fixed an error where the program only adds the number instead of doing the propper operation

--- daniel_calculator.py
+++ daniel_calculator.py
@@ -2,28 +2,28 @@ from class_calculator import Calculator
 from user_interface import UserInterface
 
 class DanielCalculator(Calculator , UserInterface):
-    def add(self, number1 , number2):
+    def add_new(self, number1 , number2):
         try:
             sum = number1 + number2
             return sum
         except ValueError:
             print("Your input must be a integer")
     
-    def minus(self, number1 , number2):
+    def minus_new(self, number1 , number2):
         try:
             difference = number1 - number2
             return difference
         except ValueError:
             print("Your input must be a integer")
     
-    def multiply(self, number1 , number2):
+    def multiply_new(self, number1 , number2):
         try:
             product = number1 * number2
             return product
         except ValueError:
             print("Your input must be a integer")
     
-    def divide(self, number1, number2):
+    def divide_new(self, number1, number2):
         try:
             quotient = number1 / number2
             return quotient
--- main_calculator.py
+++ main_calculator.py
@@ -1,24 +1,30 @@
 #call all the class that is needed
 from daniel_calculator import DanielCalculator
 #create a instance object for the classes
-daniel = DanielCalculator("Cassio")
+daniel = DanielCalculator("Calculator")
 #get the first numbers
-first_number = daniel.ask_number()
 #get the second number
-second_number = daniel.ask_number()
 #ask the user what method to use
 operation = daniel.ask_operation()
 #check using if else on what method in calculator to use
 #print the result
-if operation == "+" or "add":
-    sum = daniel.add(first_number , second_number)
+if operation == "add":
+    first_number = daniel.ask_number()
+    second_number = daniel.ask_number()
+    sum = daniel.add_new(first_number , second_number)
     daniel.print_sum(sum)
-elif operation == "-" or "minus":
-    difference = daniel.minus(first_number , second_number)
+elif operation =="minus":
+    first_number = daniel.ask_number()
+    second_number = daniel.ask_number()
+    difference = daniel.minus_new(first_number , second_number)
     daniel.print_difference(difference)
-elif operation == "*" or "multiply":
-    product = daniel.multiply(first_number, second_number)
+elif operation == "multiply":
+    first_number = daniel.ask_number()
+    second_number = daniel.ask_number()
+    product = daniel.multiply_new(first_number, second_number)
     daniel.print_product(product)
-elif operation == "/" or "divide":
-    quotient = daniel.divide(first_number , second_number)
+elif operation == "divide":
+    first_number = daniel.ask_number()
+    second_number = daniel.ask_number()
+    quotient = daniel.divide_new(first_number , second_number)
     daniel.print_quotient(quotient)
