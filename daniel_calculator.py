ea254b91f12411748ffab711e043c4c7fd09523c
Neil Daniel Vergara <vergaraneildaniel@gmail.com>
2023-06-03 20:43:13 +0800

    new minus method created

--- daniel_calculator.py
+++ daniel_calculator.py
@@ -3,5 +3,15 @@ from user_interface import UserInterface
 
 class DanielCalculator(Calculator , UserInterface):
     def add(self, number1 , number2):
-        sum = number1 + number2
-        return sum
+        try:
+            sum = number1 + number2
+            return sum
+        except ValueError:
+            print("Your input must be a integer")
+    
+    def minus(self, number1 , number2):
+        try:
+            difference = number1 - number2
+            return difference
+        except ValueError:
+            print("Your input must be a integer")
